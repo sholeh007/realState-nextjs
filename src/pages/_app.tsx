7fc1a255c502e0b19bc37f1728b08be51ea14ff9
sholeh007 <sholeh09@outlook.com>
2023-09-21 14:45:51 +0900

    update dependencies & fix some code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,55 +1,43 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { customTheme } from "../styles/theme";
-import NProgress from "nprogress";
-import Layout from "@/components/organisms/Layout";
-import Head from "next/head";
+import Layout from "@/components/organisms/Layout"
+import { ChakraProvider } from "@chakra-ui/react"
+import type { AppProps } from "next/app"
+import { useRouter } from "next/router"
+import NProgress from "nprogress"
+import { useEffect } from "react"
+import "../styles/globals.css"
+import { customTheme } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  NProgress.configure({ showSpinner: false });
+  const router = useRouter()
+  NProgress.configure({ showSpinner: false })
 
   useEffect(() => {
     const handleStart = () => {
-      NProgress.start();
-    };
+      NProgress.start()
+    }
 
     const handleStop = () => {
-      NProgress.done();
-    };
+      NProgress.done()
+    }
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleStop);
-    router.events.on("routeChangeError", handleStop);
+    router.events.on("routeChangeStart", handleStart)
+    router.events.on("routeChangeComplete", handleStop)
+    router.events.on("routeChangeError", handleStop)
 
     return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleStop);
-      router.events.off("routeChangeError", handleStop);
-    };
-  }, [router]);
+      router.events.off("routeChangeStart", handleStart)
+      router.events.off("routeChangeComplete", handleStop)
+      router.events.off("routeChangeError", handleStop)
+    }
+  }, [router])
 
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </Head>
-      <ChakraProvider theme={customTheme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </>
-  );
+    <ChakraProvider theme={customTheme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
