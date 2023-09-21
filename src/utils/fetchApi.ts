51ac373507a5cc1db468dae6f5c19790004193a9
yt-liceDev <muh.sholeh.dev@gmail.com>
2023-09-21 14:21:31 +0900

    hide host and api key

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios from "axios"
 
 export const fetchApi = async (url: string) => {
   try {
     const res = await axios.get(url, {
       headers: {
-        "x-rapidapi-host": "bayut.p.rapidapi.com",
-        "x-rapidapi-key": "6b951905dcmsh6bb43c98cee317ap15f27fjsnf56d2ea94987",
+        "x-rapidapi-host": process.env.HOST,
+        "x-rapidapi-key": process.env.API_KEY,
       },
-    });
+    })
 
-    if (!res.status) throw new Error("fetch failled");
+    if (!res.status) throw new Error("fetch failled")
 
-    return res.data;
+    return res.data
   } catch (error) {
-    return error;
+    return error
   }
-};
+}
